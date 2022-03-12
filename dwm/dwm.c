d8d04b007d4e4a26f9bf1cdcff9057bf394eaad8
U-L-M-S <uinclis@outlook.de>
2022-03-12 11:44:22 +0100

    add slstatus

--- dwm/dwm.c
+++ dwm/dwm.c
@@ -870,9 +870,7 @@ drawbar(Monitor *m)
 
 	/* draw status first so it can be overdrawn by tags later */
 	if (m == selmon) { /* status is only drawn on selected monitor */
-		drw_setscheme(drw, scheme[SchemeNorm]);
-    tw = TEXTW(stext) - lrpad / 2 + 2; /* 2px extra right padding */
-    drw_text(drw, m->ww - tw - stw, 0, tw, bh, lrpad / 2 - 2, stext, 0);
+    tw = m->ww - drawstatusbar(m, bh, stext);
 	}
 
   resizebarwin(m);
